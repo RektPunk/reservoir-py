cf12880e32afb73fad93f9d83424a2aeb5f434c4
RektPunk <rektpunk@gmail.com>
2022-11-08 17:58:48 +0900

    feat - add limit validator

--- utils/validators.py
+++ utils/validators.py
@@ -15,3 +15,17 @@ def has_value_validator(v, enum: HasValueEnum):
         return v
     else:
         raise ValueError("input not in Enum")
+
+
+def activity_limit_validator(v, values):
+    _include_metadata = values["includeMetadata"]
+    if _include_metadata:
+        if v >= 1 and v <= 20:
+            return v
+        else:
+            raise ValueError("limit not in range")
+    else:
+        if v >= 1 and v <= 1000:
+            return v
+        else:
+            raise ValueError("limit not in range")
