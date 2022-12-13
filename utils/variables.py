fd8ab11397969d83942a2d22bdc27f7d6ed7fc78
RektPunk <rektpunk@gmail.com>
2022-12-13 09:45:22 +0900

    feat - fix typo

--- utils/variables.py
+++ utils/variables.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 
 class ApiKey(Enum):
     load_dotenv()
-    RESERVIOR_API_KEY: str = os.environ["RESERVIOR_API_KEY"]
+    RESERVOIR_API_KEY: str = os.environ["RESERVOIR_API_KEY"]
 
 
 class StatusCode(int, Enum):
     _200: int = 200
 
 
-HEADERS: Dict[str, str] = {"accept": "*/*", "x-api-key": ApiKey.RESERVIOR_API_KEY.value}
+HEADERS: Dict[str, str] = {
+    "accept": "*/*",
+    "x-api-key": ApiKey.RESERVOIR_API_KEY.value,
+}
 CONTINUATION: str = "continuation"
 ACTIVITY_LIMIT_RANGE_INCLUDE_METADATA = {
     True: {
